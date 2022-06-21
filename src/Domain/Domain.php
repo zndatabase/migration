d10ee156da6c6fa5bc751e21af45389f8b4b73e2
Vitaliy <theyamshikov@yandex.ru>
2022-06-21 11:52:20 +0600

    upd

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -2,7 +2,7 @@
 
 namespace ZnDatabase\Migration\Domain;
 
-use ZnCore\Domain\Interfaces\DomainInterface;
+use ZnCore\Base\Libs\Domain\Interfaces\DomainInterface;
 
 class Domain implements DomainInterface
 {
--- src/Domain/Interfaces/Repositories/GenerateRepositoryInterface.php
+++ src/Domain/Interfaces/Repositories/GenerateRepositoryInterface.php
@@ -2,7 +2,7 @@
 
 namespace ZnDatabase\Migration\Domain\Interfaces\Repositories;
 
-use ZnCore\Domain\Interfaces\GetEntityClassInterface;
+use ZnCore\Base\Libs\Domain\Interfaces\GetEntityClassInterface;
 
 interface GenerateRepositoryInterface extends GetEntityClassInterface
 {
